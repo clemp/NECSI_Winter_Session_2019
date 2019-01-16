b1bd9a4a45bb3ee17679da74a29f4e643359fd89
christian <christianlemp@gmail.com>
2019-01-15 19:37:38 -0500

    fixed unix timestamp

--- code/twitter_network_analysis.py
+++ code/twitter_network_analysis.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import re
 
 # Create empty Graph
 G = nx.DiGraph()
@@ -25,22 +26,21 @@ with open("../data/higgs-activity_time_sample_2.txt") as f:
         # print(activity[3])
         # Only target Retweets
         if activity[3] == 'RT':
-            time = activity[2]
+            time = re.sub("[^0-9]", "", activity[2]) # remove random letters in Unix timestamp
             tweeter = activity[0]
             retweeter = activity[1]
             # Create a tuple to hold each individual node connection
             connection = tuple((tweeter, retweeter))
 
-            if time in [d['time'] for d in rt_network]:
+            if time in rt_network.keys():
                 # print("time:", time)
-                rt_network['time']['connections'].append(connection)
-                print(rt_network)
+                rt_network[time].append(connection)
+                # print(rt_network)
             else:
-                tweets_dict = dict()
-                tweets_dict['time'] = time
-                tweets_dict['connections'] = [connection]
+                rt_network[time] = [connection]
                 # Add newly generated tweet dictionary to retweet network dictionary
-                rt_network.append(tweets_dict)
+
+print(rt_network)
             # Add edges to Graph
             # G.add_edges_from([connection])
             # Print degrees of G
