1d2a6dc11e1a24802cd5268f20262e1f0cfb3f8a
christian <christianlemp@gmail.com>
2019-01-17 12:47:54 -0500

    loop through split RT files

--- code/03_degree_dist.py
+++ code/03_degree_dist.py
@@ -2,82 +2,71 @@
 import networkx as nx
 from collections import Counter
 import numpy as np
+import os
 import re
 
-# Create empty Graph
-G = nx.DiGraph()
+# Set up 'split files' data directory
+data_dir = "../data"
+
+
 
 rt_dist = []
 
-counter = 0
-# with open("../data/split_files/higgs-activity_time_sorted_RT00.txt") as f:
-with open("../data/higgs-activity_time_sorted_RT.txt") as f:
-    for line in f:
-        # split tweet activity line into list
-        # [id of tweeter, id of retweeter, timestamp, mention or retweet indicator]
-        # ^^ Double check the tweeter, retweeter id ^^
-        activity = line.strip().split()
 
-        tweeter = activity[0]
-        retweeter = activity[1]
-        # Create a tuple to hold each individual node connection
-        connection = tuple((tweeter, retweeter))
-        # print(connection)
-        G.add_edges_from([connection])
+for file in sorted(os.listdir(data_dir + "/split_files")):
+    counter = 0
+    # Create empty Graph for each file
+    G = nx.DiGraph()
+    if file.endswith(".txt"):
+        # print(file)
+        with open(data_dir + "/split_files/" + file) as f:
+        # with open("../data/higgs-activity_time_sorted_RT.txt") as f:
+            for line in f:
+                # split tweet activity line into list
+                # [id of tweeter, id of retweeter, timestamp, mention or retweet indicator]
+                # ^^ Double check the tweeter, retweeter id ^^
+                activity = line.strip().split()
 
-        # print(G.in_degree())
+                tweeter = activity[0]
+                retweeter = activity[1]
+                # Create a tuple to hold each individual node connection
+                connection = tuple((tweeter, retweeter))
+                # print(connection)
+                G.add_edges_from([connection])
 
-        counter += 1
+                # print(G.in_degree())
+                #
+                # counter += 1
+                #
+                # if counter == 20:
+                #     break
 
-        if counter == 20:
-            break
-
-f.close()
+        f.close()
 # print("# of nodes in G:", nx.number_of_nodes(G))
 # print("G.in_degree(): ", G.in_degree())
-# Create list of nodes and degrees
-degs = [d[1] for d in G.in_degree()]
-counts = list(set([(deg, degs.count(deg)) for deg in degs]))
+        # Create list of nodes and degrees
+        degs = [d[1] for d in G.in_degree()]
+        counts = list(set([(deg, degs.count(deg)) for deg in degs]))
 # counts.sort()
 # print("counts: ", counts)
 # print("sorted counts: ", sorted(counts))
 
 # [(0, 9), (1, 5), (2, 1), (3, 1)]
 
-def degree_invcumsum(dcounts):
-    # Ensure list of counts is sorted
-    dcounts = sorted(dcounts)
-
-    # Empty list to store
-    invcum_dist = []
-
-    # Enumerate tuples of (degree, number of nodes with that degree)
-    for idx, count in enumerate(dcounts):
-        # initialize empty variable to track inverse cumulative sum
-        inv_cumsum = 0
-        # print("idx: ", idx, "count: ", count)
-        # print("dcounts[idx]: ", dcounts[idx])
-        # print("dcounts[idx:]: ", dcounts[idx:])
-        # For every tuple, sum all the ndegree counts for subsequent tuples
-        for n, ndegs in dcounts[idx:]:
-            inv_cumsum += ndegs
-        # print("n: ", n, " ndegs: ", ndegs, " inv_cumsum: ", inv_cumsum)
-            # print("ndegs:", ndegs)
-        # Append inverse cumulative count to the list
-        invcum_dist.append((dcounts[idx][0], inv_cumsum))
-    # print("degree counts list: ", dcounts)
-    # print("inv cumsum: ", invcum_ dist)
-    return(invcum_dist)
-
-print("degs:", degs)
-print("counts: ", counts)
-print("Counter(degs).most_common(): ", Counter(degs).most_common())
-print("degree_invcumsum(sorted(counts)):", degree_invcumsum(sorted(counts)))
 
 
-# with open('../data/invcum_sum_output/higgs-activity_time_sorted_invcumsum_RT00.txt', 'w+') as output:
-#     output.write(str(degree_invcumsum(sorted(counts))))
-# output.close()
+    print("degs:", degs)
+    print("counts: ", counts)
+    # print("Counter(degs).most_common(): ", Counter(degs).most_common())
+    # print("degree_invcumsum(sorted(counts)):", degree_invcumsum(sorted(counts)))
+    # print("degree_invcumsum(Counter(degs).most_common()):", degree_invcumsum(Counter(degs).most_common()))
+
+
+
+    with open(data_dir + "/split_files/node_dists/node_dist" + file, 'w+') as output:
+        # output.write(str(degree_invcumsum(sorted(counts))))
+        output.write(str(degs))
+    output.close()
 # print(degree_invcumsum(sorted(counts)))
 # For each tuple, sum number of degrees for each subsequent tuple in sorted degree counts list
 
