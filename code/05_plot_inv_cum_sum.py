967c22bc9215d0963dc18bbe6ae344ff01cfee60
christian <christianlemp@gmail.com>
2019-01-17 17:08:53 -0500

    added labels

--- code/05_plot_inv_cum_sum.py
+++ code/05_plot_inv_cum_sum.py
@@ -12,15 +12,22 @@ y1 = [d[1] for d in test_icsd1[1:]]
 x2 = [d[0] for d in test_icsd2[1:]]
 y2 = [d[1] for d in test_icsd2[1:]]
 
+# Plot properties
+a = 0.6
+
 ax = plt.gca()
 # Configure plot figure
 plt.figure(figsize=(13, 8)) # adjust figure size
 # plt.plot(x, y, 'g-', label='model') # plot with green line
 
-plt.scatter(x1, y1, s=4, alpha = 0.4)
-plt.scatter(x2, y2, s=4, alpha = 0.4)
-plt.xscale("log")
+plt.scatter(x1, y1, s=4, alpha = a, label='# RTs <= 35k')
+plt.scatter(x2, y2, s=4, alpha = a, label='35k < # RTs <= 70k')
 
+plt.ylabel(r"$inverse cum. sum of <Kin>$", size = 18) # add LaTeX label on y-axis, increase size
+plt.xlabel(r"$num. nodes$", size = 18) # add label on x-axis, increase size
+plt.xscale("log")
 plt.yscale("log")
+
+plt.legend(loc='best') # add legend for labels
+plt.tight_layout() # fit chart to output figure
 plt.show()
-print(print(x))
