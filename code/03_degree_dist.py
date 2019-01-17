fdb15b84a1bfb7ffc5be880e424e691d43b26a90
christian <christianlemp@gmail.com>
2019-01-17 05:55:01 -0500

    added inverse cumulative sum function

--- code/03_degree_dist.py
+++ code/03_degree_dist.py
@@ -1,6 +1,7 @@
 # https://snap.stanford.edu/data/higgs-twitter.html
 import networkx as nx
 from collections import Counter
+import numpy as np
 import re
 
 # Create empty Graph
@@ -32,18 +33,51 @@ with open("../data/split_files/higgs-activity_time_sorted_RT00.txt") as f:
 
 f.close()
 print("# of nodes in G:", nx.number_of_nodes(G))
-
+print("G.in_degree(): ", G.in_degree())
 # Create list of nodes and degrees
 degs = [d[1] for d in G.in_degree()]
 counts = list(set([(deg, degs.count(deg)) for deg in degs]))
-counts.sort()
-print(counts)
-print("degs: ", degs)
+# counts.sort()
+# print("counts: ", counts)
+print("sorted counts: ", sorted(counts))
+
+# [(0, 9), (1, 5), (2, 1), (3, 1)]
+
+def degree_invcumsum(dcounts):
+    # Ensure list of counts is sorted
+    dcounts = sorted(dcounts)
+    
+    # Empty list to store
+    invcum_dist = []
+
+    # Enumerate tuples of (degree, number of nodes with that degree)
+    for idx, count in enumerate(dcounts):
+        # initialize empty variable to track inverse cumulative sum
+        inv_cumsum = 0
+        print("idx: ", idx, "count: ", count)
+        print("dcounts[idx]: ", dcounts[idx])
+        print("dcounts[idx:]: ", dcounts[idx:])
+        # For every tuple, sum all the ndegree counts for subsequent tuples
+        for n, ndegs in dcounts[idx:]:
+            inv_cumsum += ndegs
+        print("n: ", n, " ndegs: ", ndegs, " inv_cumsum: ", inv_cumsum)
+            # print("ndegs:", ndegs)
+        # Append inverse cumulative count to the list
+        invcum_dist.append((dcounts[idx][0], inv_cumsum))
+    print(invcum_dist)
+    return(invcum_dist)
+degree_invcumsum(sorted(counts))
+# For each tuple, sum number of degrees for each subsequent tuple in sorted degree counts list
+
+# print("degs: ", degs)
 # Create x, y values for plotting
 # x = possible degrees a node can have
 # y = number of nodes with degree >= x
 # We will create plot of log(x), log(y)
 
 # Create list of all possible degrees, from min to max
-x = list(range(max(degs)+1))
-print("x: ", x)
+#x = list(range(max(degs)+1))
+# x = [count[0] for count in counts]
+# Create list of inverse cumulative degree distributions
+# y = [sum([c[1] for c in counts[i:]]) for i in range(len(counts))]
+# print("x: ", x)
