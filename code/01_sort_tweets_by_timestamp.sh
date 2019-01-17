cd022ab47d0fe17f3e1337b47176d22bc8ff0331
christian <christianlemp@gmail.com>
2019-01-16 19:44:46 -0500

    filter to include only RT records

--- code/01_sort_tweets_by_timestamp.sh
+++ code/01_sort_tweets_by_timestamp.sh
@@ -1,4 +1,6 @@
 cd ../data
 # awk '{ gsub(/[a-zA-Z]/,"", $3)}' higgs-activity_time_sample.txt > higgs-activity_time_sample_clean.txt
 # sort -nk3 higgs-activity_time_sample_clean.txt > higgs-activity_time_sample_clean_sorted.txt
-sort -nk3 higgs-activity_time_sample.txt > higgs-activity_time_sample_sorted.txt
+sort -nk3 higgs-activity_time.txt > higgs-activity_time_sorted.txt
+
+grep RT higgs-activity_time_sorted.txt > higgs-activity_time_sorted_RT.txt
